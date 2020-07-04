8cdf2fb32c2a291b141ea0045279c5be0f516955
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-07-03 17:13:32 -0700

    Updates

---
+++ precache-manifest.b28aa7fbbf5cca748c305d042dc71111.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eafa46b3e1a1dc54d9f9e1c0703c20",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7cfaf005f2da80912d78",
+    "url": "/weather-app/static/css/main.5f4d8b10.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/weather-app/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfaf005f2da80912d78",
+    "url": "/weather-app/static/js/main.a77d47e8.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  },
+  {
+    "revision": "72e61ede1069f7727fcde52173ac6462",
+    "url": "/weather-app/static/media/cloudy-bg.72e61ede.png"
+  },
+  {
+    "revision": "4d6707f9cb36038eb98d5fd1696537aa",
+    "url": "/weather-app/static/media/cold-bg.4d6707f9.png"
+  },
+  {
+    "revision": "2af61c8bcfbcd5da2f8209ab0fc5920e",
+    "url": "/weather-app/static/media/sunny-bg.2af61c8b.png"
+  },
+  {
+    "revision": "e12c7eebdd13707c6cdd071151735439",
+    "url": "/weather-app/static/media/warm-bg.e12c7eeb.png"
+  }
+]);
